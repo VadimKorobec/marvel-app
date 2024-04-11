d4709e4dc77c561877ab298f3139be17c903d88b
VadimKorobec <_s_e_t__@ukr.net>
2024-04-11 14:32:26 +0300

    add some code

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,42 +1,28 @@
-import { useState } from "react";
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
-import decoration from "../../resources/img/vision.png";
-import AppBanner from "../appBanner/AppBanner";
-import ComicsList from "../comicsList/ComicsList";
+import MainPage from "../../pages/MainPage";
+import ComicsPage from "../../pages/ComicsPage";
 
 const App = () => {
-  const [selectedChar, setChar] = useState(null);
-
-  const onCharSelected = (id) => {
-    setChar(id);
-  };
-
   return (
     <BrowserRouter>
       <div className="app">
         <AppHeader />
         <main>
-          <ErrorBoundary>
-            <RandomChar />
-          </ErrorBoundary>
-          <div className="char__content">
-            <ErrorBoundary>
-              <CharList onCharSelected={onCharSelected} />
-            </ErrorBoundary>
-            <ErrorBoundary>
-              <CharInfo charId={selectedChar} />
-            </ErrorBoundary>
-          </div>
-          <img className="bg-decoration" src={decoration} alt="vision" />
-          <AppBanner />
-          <ComicsList />
+          <Switch>
+            <Route exact path="/comics">
+              <ComicsPage />
+            </Route>
+            <Route exact path="/">
+              <MainPage />
+            </Route>
+          </Switch>
         </main>
       </div>
     </BrowserRouter>
