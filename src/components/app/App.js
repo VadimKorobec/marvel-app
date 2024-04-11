d68f74cfac3d8b33c485bfb773776e60709ec303
VadimKorobec <_s_e_t__@ukr.net>
2024-04-11 13:26:12 +0300

    add some code

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,45 +1,45 @@
-// import { useState } from "react";
+import { useState } from "react";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom";
 
-import { TestComponent } from "../testComponent/TestComponent";
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
-// import AppHeader from "../appHeader/AppHeader";
-// import RandomChar from "../randomChar/RandomChar";
-// import CharList from "../charList/CharList";
-// import CharInfo from "../charInfo/CharInfo";
-// import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-// import decoration from "../../resources/img/vision.png";
-// import AppBanner from "../appBanner/AppBanner";
-// import ComicsList from "../comicsList/ComicsList";
+import decoration from "../../resources/img/vision.png";
+import AppBanner from "../appBanner/AppBanner";
+import ComicsList from "../comicsList/ComicsList";
 
 const App = () => {
-  // const [selectedChar, setChar] = useState(null);
+  const [selectedChar, setChar] = useState(null);
 
-  // const onCharSelected = (id) => {
-  //   setChar(id);
-  // };
+  const onCharSelected = (id) => {
+    setChar(id);
+  };
 
   return (
-    <div className="app">
-      <TestComponent />
-      {/* <AppHeader />
-      <main>
-        <ErrorBoundary>
-          <RandomChar />
-        </ErrorBoundary>
-        <div className="char__content">
+    <BrowserRouter>
+      <div className="app">
+        <AppHeader />
+        <main>
           <ErrorBoundary>
-            <CharList onCharSelected={onCharSelected} />
+            <RandomChar />
           </ErrorBoundary>
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar} />
-          </ErrorBoundary>
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" />
-        <AppBanner />
-        <ComicsList />
-      </main> */}
-    </div>
+          <div className="char__content">
+            <ErrorBoundary>
+              <CharList onCharSelected={onCharSelected} />
+            </ErrorBoundary>
+            <ErrorBoundary>
+              <CharInfo charId={selectedChar} />
+            </ErrorBoundary>
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision" />
+          <AppBanner />
+          <ComicsList />
+        </main>
+      </div>
+    </BrowserRouter>
   );
 };
 
