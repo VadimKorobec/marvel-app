8c6a48c6fd240d18a79a513376593e787c4b7550
VadimKorobec <_s_e_t__@ukr.net>
2024-04-12 15:33:55 +0300

    add some code

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,23 +1,27 @@
 import { Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
 
-import MainPage from "../../pages/MainPage";
-import ComicsPage from "../../pages/ComicsPage";
-import NotFound from "../../pages/NotFound";
-import SingleComicPage from "../../pages/SingleComicPage";
+const NotFound = lazy(() => import("../../pages/NotFound"));
+const MainPage = lazy(() => import("../../pages/MainPage"));
+const ComicsPage = lazy(() => import("../../pages/ComicsPage"));
+const SingleComicPage = lazy(() => import("../../pages/SingleComicPage"));
 
 const App = () => {
   return (
     <div className="app">
       <AppHeader />
       <main>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/comics" element={<ComicsPage />} />
-          <Route path="/comics/:comicId" element={<SingleComicPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/comics" element={<ComicsPage />} />
+            <Route path="/comics/:comicId" element={<SingleComicPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </main>
     </div>
   );
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,7 +4,6 @@ const useMarvelService = () => {
   const { loading, request, error, clearError } = useHttp();
 
   const _apiBase = "https://gateway.marvel.com:443/v1/public/";
-  // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
   const _apiKey = "apikey=94233dbed83f30ea420bcda4b0d99b71";
   const _baseOffset = 210;
 
@@ -56,7 +55,6 @@ const useMarvelService = () => {
         : "No information about the number of pages",
       thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
       language: comics.textObjects[0]?.language || "en-us",
-      // optional chaining operator
       price: comics.prices[0].price
         ? `${comics.prices[0].price}$`
         : "not available",
